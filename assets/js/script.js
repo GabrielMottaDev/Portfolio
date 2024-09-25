4821d171a0884d2750ac11c414f64585772df661
GabrielMottaDev <gabrielmottadev@gmail.com>
2024-09-25 19:09:46 -0300

    Revert "Fix: Birth date now considers month diff"
    This reverts commit 4e15ae7c985b35a2cbb36389766e1793ee823eaf.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,11 @@ function changeYears(){
 function changeAges(){
     let birthDate = new Date("07/02/2002");
     let date = new Date();
-    let age = date.getFullYear() - birthDate.getFullYear();
-    let monthDiff = date.getMonth() - birthDate.getMonth();
-    let dayDiff = date.getDate() - birthDate.getDate();
-
-    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-        age--;
-    }
-    
+    let years = (date.getTime()-birthDate.getTime())/1000/60/60/24/30/12;
+    years = Math.floor(years);
     let agesObj = document.getElementsByClassName("js-age");
     for(let i = 0; i < agesObj.length; i++){
         let ageObj = agesObj[i];
-        ageObj.innerText = age;
+        ageObj.innerText = years;
     }
 }
